983402e79df6e3816aace51b842140edd60bfe18
rlneto <rlneto@live.com>
2023-10-08 14:50:42 -0300

    corrigido

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const CADASTRO = "https://estagios-ine-api.onrender.com/api/v1/auth/cadastro";
 const LOGIN = "https://estagios-ine-api.onrender.com/api/v1/auth/login";
 
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-const senha = document.getElementById("senha");
-const entrar = document.getElementById("entrar");
-const cadastrar = document.getElementById("cadastrar");
+const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+const senha = document.querySelector("#senha");
+const entrar = document.querySelector("#entrar");
+const cadastrar = document.querySelector("#cadastrar");
 
 entrar.addEventListener("click", async () => {
 	try {
@@ -14,7 +14,7 @@ entrar.addEventListener("click", async () => {
 			senha: senha.value,
 		});
 		sessionStorage.setItem("token", data.token);
-		const logout = document.getElementById("#logout");
+		const logout = document.querySelector("#logout");
 		logout.classList.remove("hidden");
 		const body = document.querySelector("body");
 		const boasvindas = document.createElement("h1");
